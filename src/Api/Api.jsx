6714233e4a0f8c7af0a9e2a332cddea188a93d87
SemSlipchenko <semslipchenko@gmail.com>
2023-03-27 20:47:12 +0300

    update pages

---
+++ src/Api/Api.jsx
@@ -0,0 +1,1 @@
+const ApiKey = 'baad8a66bbb439ad3cd4804fee431eda';
--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,9 +1,7 @@
 export const Movies = () => {
   return (
     <main>
-      <h1>Welcome</h1>
-      <img src="https://via.placeholder.com/960x240" alt="" />
-      <p>Movies</p>
+      <h1>Movies</h1>
     </main>
   );
 };
